bb2f47a9dd4c5e66581c1efcf2d6a2391ba12145
Friedrich Romstedt <friedrichromstedt@gmail.com>
2021-12-16 19:01:41 +0100

    First approaches to the State Machine of the airlock
    Introducing some drafts of functions to detect liquids at the three
    POIs: left, centre and right.  I am distinguishing between:
    
    1.  Not filled at all (filling degree 0);
    2.  partially filled (filling degree 1);
    3.  full (filling degree 2).
    
    Providing enum-like local variables for the specific pixel addresses,
    the aim, and the possible aims stored therein.

--- Clonk Objects/Airlock.c4d/Script.c
+++ Clonk Objects/Airlock.c4d/Script.c
@@ -1,5 +1,50 @@
 #strict 2
 
+local top, bottom, left, right;
+
+local aim;
+    local doPowerOff;
+    local doClose, doOpenLeft, doOpenRight, doOpenBoth,
+        doForcedOpenLeft, doForcedOpenRight, doForcedOpenBoth;
+    local doTransfer;
+    local doDryPumpLeft, doDryPumpRight;
+
+func fillingDegree(int x) {
+    if (GBackLiquid(x, top)) {
+        return(2);
+    } else if (GBackLiquid(x, bottom)) {
+        return(1);
+    } else {
+        return(0);
+    }
+}
+
+func getMaterial(int x) {
+    if (GBackLiquid(x, top)) {
+        return(GetMaterial(x, top));
+    } else if (GBackLiquid(x, bottom)) {
+        return(GetMaterial(x, bottom));
+    }
+    return(-1);
+}
+
 func Initialize() {
-    SetAction("DryPumpingRight");
+    top = -10;
+    bottom = 11;
+    left = -80;
+    right = 80;
+
+    doPowerOff = 0;
+    doClose = 1;
+    doOpenLeft = 2;
+    doOpenRight = 3;
+    doOpenBoth = 4;
+    doTransfer = 5;
+    doDryPumpLeft = 6;
+    doDryPumpRight = 7;
+    doForcedOpenLeft = 8;
+    doForcedOpenRight = 9;
+    doForcedOpenBoth = 10;
+
+    SetAction("Off");
 }
