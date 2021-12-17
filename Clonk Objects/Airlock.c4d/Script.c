652f53b91f64cf1b148fea7d09f9273c0a195a87
Friedrich Romstedt <friedrichromstedt@gmail.com>
2021-12-17 19:33:19 +0100

    First work on the state machine
    Structure:
    
    1.  There are inifite Actions: Idling, Off, IdlingClosedLeft,
        IdlingClosedRight, PumpLeftOut, PumpLeftIn, PumpRightOut,
        PumpRightIn, WalkRightOpenLeft, WalkRightOpenRight,
        WalkLeftOpenLeft, WalkLeftOpenRight, DryPumpingLeft,
        DryPumpingRight.
    
    2.  There is a transition function "Run" which leaves these infinite
        Actions based on the aim.  It will also perform the pumping.
    
    3.  There will be End functions called when a non-infinite Action stops.
        These functions will decide which Action to do next.  PowerOnDone is
        an example.
    
    Actions added here: IdlingClosedLeft, IdlingClosedRight,
    ShieldOpenLeftII, ShieldCloseLeftII, ShieldOpenRightII,
    ShieldCloseRightII.  The "...II" Action act with the resp. other shield
    *opened*.
    
    Added the Run function as the TimeCall of the Object with Timer=1.
    
    Some *aims* have been renamed from "do..." to "be...".  Some remained as
    "do...".
    
    The Run function currently handles: Off, Idling, IdlingClosedLeft,
    IdlingClosedRight.  It needs to cover *all* infinite Actions (see
    above).
    
    The PowerOnDone function propagates towards: bePoweredOff, beClosed,
    beOpenLeft, beOpenRight, beOpen.
    
    There is redundancy between the "off" state and the "open" state.  Both
    look the same.  Internally I am using only the Off notion; however it
    might be useful to present "open" as an option to the user.
    
    The first development target is to support opening/closing shields.
    These are the aims: bePoweredOff, beClosed, beOpenLeft, beOpenRight,
    beOpen.

--- Clonk Objects/Airlock.c4d/Script.c
+++ Clonk Objects/Airlock.c4d/Script.c
@@ -3,9 +3,9 @@
 local top, bottom, left, right;
 
 local aim;
-    local doPowerOff;
-    local doClose, doOpenLeft, doOpenRight, doOpenBoth,
-        doForcedOpenLeft, doForcedOpenRight, doForcedOpenBoth;
+    local bePoweredOff;
+    local beClosed, beOpenLeft, beOpenRight, beOpen,
+        beForcedOpenLeft, beForcedOpenRight, beForcedOpen;
     local doTransfer;
     local doDryPumpLeft, doDryPumpRight;
 
@@ -28,23 +28,81 @@ func getMaterial(int x) {
     return(-1);
 }
 
+func Run() {
+    var current_action = GetAction();
+    if (current_action == "Off") {
+        if (aim == beClosed) {
+            SetAction("PowerOn");
+        } else if (aim == beOpenLeft) {
+            SetAction("ShieldCloseRightII");
+        } else if (aim == beOpenRight) {
+            SetAction("ShieldCloseLeftII");
+        }
+    } else if (current_action == "Idling") {
+        if (aim == bePoweredOff) {
+            SetAction("PowerOff");
+        } else if (aim == beOpenLeft) {
+            SetAction("ShieldOpenLeft");
+        } else if (aim == beOpenRigth) {
+            SetAction("ShieldOpenRight");
+        } else if (aim == beOpen) {
+            SetAction("PowerOff");
+        }
+    } else if (current_action == "IdlingClosedLeft") {
+        if (aim == bePoweredOff) {
+            SetAction("ShieldOpenLeftII");
+        } else if (aim == beClosed) {
+            SetAction("ShieldCloseRight");
+        } else if (aim == beOpenLeft) {
+            SetAction("ShieldCloseRight");
+        } else if (aim == beOpen) {
+            SetAction("ShieldOpenLeftII");
+        }
+    } else if (current_action == "IdlingClosedRight") {
+        if (aim == bePoweredOff) {
+            SetAction("ShieldOpenRightII");
+        } else if (aim == beClosed) {
+            SetAction("ShieldCloseLeft");
+        } else if (aim == beOpenRight) {
+            SetAction("ShieldCloseLeft");
+        } else if (aim == beOpen) {
+            SetAction("ShieldOpenRightII");
+        }
+    }
+}
+
+func PowerOnDone() {
+    if (aim == bePoweredOff) {
+        SetAction("PowerOff");
+    } else if (aim == beClosed) {
+        SetAction("Idling");
+    } else if (aim == beOpenLeft) {
+        SetAction("ShieldOpenLeft");
+    } else if (aim == beOpenRight) {
+        SetAction("ShieldOpenRight");
+    } else if (aim == beOpen) {
+        SetAction("PowerOff");
+    }
+}
+
 func Initialize() {
     top = -10;
     bottom = 11;
     left = -80;
     right = 80;
 
-    doPowerOff = 0;
-    doClose = 1;
-    doOpenLeft = 2;
-    doOpenRight = 3;
-    doOpenBoth = 4;
+    bePoweredOff = 0;
+    beClosed = 1;
+    beOpenLeft = 2;
+    beOpenRight = 3;
+    beOpen = 4;
     doTransfer = 5;
     doDryPumpLeft = 6;
     doDryPumpRight = 7;
-    doForcedOpenLeft = 8;
-    doForcedOpenRight = 9;
-    doForcedOpenBoth = 10;
+    beForcedOpenLeft = 8;
+    beForcedOpenRight = 9;
+    beForcedOpen = 10;
 
     SetAction("Off");
+    aim = bePoweredOff;
 }
